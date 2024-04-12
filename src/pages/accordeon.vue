9b4ce5fbde2a273ac4540b92cb9653b760a00e3f
Barder_Lucas <bardey.estelle1@gmail.com>
2024-04-12 08:49:23 +0200

    Interactif menu

--- src/pages/accordeon.vue
+++ src/pages/accordeon.vue
@@ -6,17 +6,22 @@ const sectionOpen = ref(1)
 
 <template>
     <section>
-        <button class="text-xl" @pointerdown="sectionOpen==1">bouton 1</button>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis, incidunt eveniet atque nesciunt eligendi inventore? Quas ut ab mollitia eligendi, eum repellendus laudantium esse cumque fugit laborum excepturi officia eaque.</p>
+        <button class="text-xl" @pointerdown="sectionOpen = 1">bouton 1</button>
+        <p v-show="sectionOpen == 1">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis, incidunt
+            eveniet atque nesciunt
+            eligendi inventore? Quas ut ab mollitia eligendi, eum repellendus laudantium esse cumque fugit laborum
+            excepturi officia eaque.</p>
     </section>
     <section>
-        <button class=" text-xl">bouton 2</button>
-        <p>Ut tenetur nam dolore. Accusamus reiciendis iusto eligendi numquam facilis dolores. Ex, unde reiciendis eos,
+        <button class="text-xl" @pointerdown="sectionOpen = 2">bouton 2</button>
+        <p v-show="sectionOpen == 2">Ut tenetur nam dolore. Accusamus reiciendis iusto eligendi numquam facilis dolores.
+            Ex, unde reiciendis eos,
             autem officia amet mollitia harum ullam fugiat atque, odio nemo impedit maiores officiis aut sunt!</p>
     </section>
     <section>
-        <button class="text-xl">bouton 3</button>
-        <p>Impedit tempora dolores esse pariatur temporibus nam! Tempora molestias eius voluptatibus enim, ducimus dicta
+        <button class="text-xl" @pointerdown="sectionOpen = 3">bouton 3</button>
+        <p v-show="sectionOpen == 3">Impedit tempora dolores esse pariatur temporibus nam! Tempora molestias eius
+            voluptatibus enim, ducimus dicta
             laborum suscipit possimus neque a quo dignissimos expedita quae sit sed voluptates magni inventore natus at.
         </p>
     </section>
